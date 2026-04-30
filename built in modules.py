a7480ae9ee1cf799d47bf392e4a8187b67a88a59
Gayatri123632 <gayatriyadala2008@gmail.com>
2026-04-30 11:37:42 +0530

    Add files via upload

---
+++ built in modules.py
@@ -0,0 +1,12 @@
+#built in modules:
+#datetime
+import datetime
+print(datetime.datetime.now())
+#random
+import random
+print(random.randint(1,10))
+#math
+import math 
+print(math.sqrt(100))
+#os
+
---
+++ copy.py
@@ -0,0 +1,5 @@
+fruits=["apple","banana","cherry"]
+print(fruits)
+fruitlist=[]
+fruitlist=fruits.copy()
+print(fruitlist)
