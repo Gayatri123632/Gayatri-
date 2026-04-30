0d7e9dfd264f33abbaadafb4096446635c6e6df5
Gayatri123632 <gayatriyadala2008@gmail.com>
2026-04-30 11:24:45 +0530

    armstrong number

---
+++ armstrong number.py
@@ -0,0 +1,22 @@
+num=int(input("enter the value"))
+temp=num
+sum=0
+while temp>0:
+    digit=temp%10
+    sum=sum+digit**3
+    temp=temp//10
+if num==sum:
+    print("armstrong number")
+else:
+    print("not an armstrong number")
+
+
+
+
+
+
+
+
+
+
+
